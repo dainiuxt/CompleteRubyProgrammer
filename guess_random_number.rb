5c62a4bb623eee74704e64f72c52a8afbf900d2a
Dainius <dainiuxt@users.noreply.github.com>
2020-07-23 13:11:20 +0300

    Update guess_random_number.rb
    Added user input validation with error messages

--- guess_random_number.rb
+++ guess_random_number.rb
@@ -1,8 +1,15 @@
 # User try to guess randomly generated number in a range 0-3
+match = rand 4
+puts "Welcome to number guessing game."
 print "Please enter a number between 0 and 3: "
 user_number = gets.chomp.to_i
-match = rand 4
-if user_number == match 
-  puts "Bingo, you guessed it!"
-else puts "Please try again, sorry..."
+while user_number != match
+  if user_number > 3 || user_number < 0
+    print "Number should be in a range [0..3]: "
+    user_number = gets.chomp.to_i
+  elsif user_number <= 3 && user_number >= 0 && user_number != match
+    print "Please try again: "
+    user_number = gets.chomp.to_i
+  end
 end
+puts "Bingo, you guessed it!"
