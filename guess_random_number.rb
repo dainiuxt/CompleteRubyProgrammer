a86548837aa3f62537e3ed2274075bc9f6d911fd
Dainius <dainiuxt@users.noreply.github.com>
2020-07-23 13:50:25 +0300

    Update guess_random_number.rb
    Extended available guess range, added attempts count with pluralization.

--- guess_random_number.rb
+++ guess_random_number.rb
@@ -1,15 +1,18 @@
-# User try to guess randomly generated number in a range 0-3
-match = rand 4
+# User try to guess randomly generated number in a range 0-9
+require 'active_support/inflector'
+match = rand 10
+i = 1
 puts "Welcome to number guessing game."
-print "Please enter a number between 0 and 3: "
+print "Please enter a number between 0 and 9: "
 user_number = gets.chomp.to_i
 while user_number != match
-  if user_number > 3 || user_number < 0
-    print "Number should be in a range [0..3]: "
+  if user_number > 9 || user_number < 0
+    print "Number should be in a range [0..9]: "
     user_number = gets.chomp.to_i
-  elsif user_number <= 3 && user_number >= 0 && user_number != match
+  elsif user_number <= 9 && user_number >= 0 && user_number != match
     print "Please try again: "
     user_number = gets.chomp.to_i
+    i += 1
   end
 end
-puts "Bingo, you guessed it!"
+puts "Bingo, you guessed it in #{i} " + "attempt".pluralize(i) + "!"
