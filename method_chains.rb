aee4f89fbf67f887ad34fd8c954b1572c896bba5
Dainius <dainiuxt@users.noreply.github.com>
2020-07-24 15:18:00 +0300

    Update method_chains.rb

--- method_chains.rb
+++ method_chains.rb
@@ -1,13 +1,15 @@
-    # Calling one method in another without "foo bar baz" b/s.
-    # Just to make developer happy and expression understandable 
-    # for ones who are speaking human language... :)
-    
-    def congratulations
-      return "hello "
-    end
-     
-    def person(name = "noname")
-      return name
-    end
-     
-    puts congratulations + person("Dainius")
+# Calling one method in another without "foo bar baz" b/s.
+# Just to make developer happy and expression understandable 
+# for ones who are speaking human language... :)
+
+def congratulations
+  return "hello "
+end
+ 
+def person(name = "noname")
+  return name
+end
+ 
+puts congratulations + person("Dainius") # output: "hello Dainius"
+
+puts congratulations + person # output: "hello noname"
