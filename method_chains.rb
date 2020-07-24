c08fe482ae3489bd64b813d7328e05ae363a9812
Dainius <dainiuxt@users.noreply.github.com>
2020-07-24 15:16:00 +0300

    Create method_chains.rb

---
+++ method_chains.rb
@@ -0,0 +1,13 @@
+    # Calling one method in another without "foo bar baz" b/s.
+    # Just to make developer happy and expression understandable 
+    # for ones who are speaking human language... :)
+    
+    def congratulations
+      return "hello "
+    end
+     
+    def person(name = "noname")
+      return name
+    end
+     
+    puts congratulations + person("Dainius")
